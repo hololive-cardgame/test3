51e3dc03bfb267e6d59d2cc297e0de93b1ae4f5e
hololive-cardgame <ejo3ru@gmail.com>
2025-03-23 10:27:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const keywordSelect = document.getElementById('keyword');  // 關鍵字
+
 let filteredCards = [];  // 篩選後的卡牌資料
 
 // 使用 fetch 從 JSON 檔案載入資料
@@ -13,3 +15,47 @@ fetch('cards.json')
     });
 
 // 根據 JSON 資料生成篩選選項
+function generateFilterOptions() {
+    const keywords = new Set();
+    const types = new Set();
+    const attributes = new Set();
+    const tags = new Set();
+    const sets = {
+        "起始牌組": new Set(),
+        "補充包": new Set(),
+        "其他": new Set()
+    };
+
+    // 這是用來儲存卡牌名稱的集合
+    cardsData.forEach(card => {
+        keywords.add(card.name);
+        types.add(card.type);
+        attributes.add(card.attribute);
+        if (card.tag) {
+            card.tag.split(' / ').forEach(tag => tags.add(tag));
+        }
+        if (card.set) {
+            if (card.set.includes("起始牌組")) {
+                sets["起始牌組"].add(card.set);
+            }else if (card.set.includes("補充包")) {
+                sets["補充包"].add(card.set);
+            }else if (card.set === "配件" || card.set === "PR卡"){
+                sets["其他"].add(card.set);
+            }
+        }
+    });
+
+    // 填充關鍵字選項
+    keywords.forEach(keyword => {
+        if (keyword) {
+            const option = document.createElement('option');
+            option.value = keyword;
+            option.textContent = keyword;
+            keywordSelect.appendChild(option);
+        }
+    });
+    // 初始化關鍵字 Select2
+    $(document).ready(function() {
+        $('#keyword').select2();  // 初始化 Select2
+    });
+}
